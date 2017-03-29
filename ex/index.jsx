0bd4f04cfd3233fbfe48975330050a09fca77d69
andersonlemos <anderson.lemos@dmorj.com.br>
2017-03-29 17:54:43 -0300

    update redux fieldActions

--- ex/index.jsx
+++ ex/index.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {combineReducers,createStore} from 'redux'
 import {Provider} from 'react-redux'
-
 import Field from './field'
+import fieldReducer from './fieldReducer'
 
 const reducers = combineReducers({
-        field:()=>({value:'Opa'})
+        field:fieldReducer
 })
-
 
 ReactDOM.render(
         <Provider store={createStore(reducers)}>
