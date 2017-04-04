de7041c46af255d3b35032518f12d016c516fb6d
andersonlemos <anderson.lemos@dmorj.com.br>
2017-04-04 08:16:58 -0300

    created reducers

--- ex/counter.jsx
+++ ex/counter.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
+import {inc,dec,stepChanged} from './counterActions.js'
 
 const Counter = props =>(
-    <div>OK</div>      
+    <div>
+        <h1>{props.counter.number}</h1>
+        <input onChange={props.stepChanged} value={props.counter.step} type='number'/>
+        <button onClick={props.inc}>INC</button>
+        <button onClick={props.dec}>DEC</button>
+    </div>    
 )
 
-export default Counter
+const mapStateToProps = state =>({ counter : state.counter })
+const mapDispatchToProps = dispatch => bindActionCreators({
+    inc,
+    dec,
+    stepChanged
+},dispatch)
+
+export default connect(mapStateToProps,mapDispatchToProps)(Counter)
--- ex/counterReducer.js
+++ ex/counterReducer.js
@@ -4,10 +4,12 @@ export default function(state= INITIAL_STATE,action){
 
         switch(action.type){
             case 'INC':
-                     return {...state,number:state.number + state.step}
+                     return {...state, number : state.number + state.step}
             case 'DEC':
-                     return {...state,number:state.number - state.step}
+                     return {...state, number : state.number - state.step}
+            case 'STEP_CHANGED':
+                     return {...state, step : +action.payload}
             default:
-                     return {state}
+                     return state
         }
 }
